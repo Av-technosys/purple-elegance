19236634ec1a104c739fb5098d138296d3137f92
piyush khare <piyushkharejhs@gmail.com>
2026-07-22 16:53:36 +0530

    comment out unused environment variables in env.ts

--- config/env.ts
+++ config/env.ts
@@ -18,10 +18,10 @@ export const SES_FROM_EMAIL = process.env.SES_FROM_EMAIL!
 export const SES_FROM_NAME = process.env.SES_FROM_NAME!
 
 //  AWS S3 
-export const S3_BUCKET_NAME = process.env.S3_BUCKET_NAME!
-export const S3_BUCKET_REGION = process.env.S3_BUCKET_REGION!
-export const S3_PUBLIC_URL = process.env.S3_PUBLIC_URL!
+// export const S3_BUCKET_NAME = process.env.S3_BUCKET_NAME!
+// export const S3_BUCKET_REGION = process.env.S3_BUCKET_REGION!
+// export const S3_PUBLIC_URL = process.env.S3_PUBLIC_URL!
 
 //  App 
-export const APP_URL = process.env.NEXT_PUBLIC_APP_URL!
+// export const APP_URL = process.env.NEXT_PUBLIC_APP_URL!
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME!
